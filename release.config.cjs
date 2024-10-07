66b173d78336b4d9102c9241994c4f907d63ce90
Luiz Gustavo Saraiva <lg.saraiva@outlook.com>
2024-10-07 20:03:53 -0300

    fix(semantic-release): fix config file

--- release.config.cjs
+++ release.config.cjs
@@ -4,4 +4,20 @@
 module.exports = {
   branches: ["main"],
   repositoryUrl: "https://github.com/luizgustavosaraiva/get-pull-requests-commits",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+  ],
 };
